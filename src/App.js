770bc2c84e2b0b0f83f35074c6ac035d309b4a96
Al Joseph Condino <condinoaljoseph@gmail.com>
2020-02-19 18:49:58 +0800

    commit for debug only

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from './component/Header/Header';
-import About from './component/Sections/About/About';
-import Features from './component/Sections/Features/Features';
-import Projects from './component/Sections/Projects/Projects';
-import Footer from './component/Footer/Footer';
+// import Header from './component/Header/Header';
+// import About from './component/Sections/About/About';
+// import Features from './component/Sections/Features/Features';
+// import Projects from './component/Sections/Projects/Projects';
+// import Footer from './component/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
+      {/* <Header />
 
       <main>
         <About />
@@ -17,7 +17,8 @@ function App() {
         <Projects />
       </main>
 
-      <Footer />
+      <Footer /> */}
+      Hello world
     </div>
   );
 }
