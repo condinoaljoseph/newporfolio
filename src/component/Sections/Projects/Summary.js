f2111b4a3fb821d3a812ea09c73c2096af7536c8
Al Joseph Condino <condinoaljoseph@gmail.com>
2020-02-21 13:08:11 +0800

    fixed modal state

--- src/component/Sections/Projects/Summary.js
+++ src/component/Sections/Projects/Summary.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import Spinner from '../../UI/Spinner';
 
 const Summary = ({ content }) => {
+  const [project, setProject] = useState({
+    title: null,
+    details: null,
+    description: null,
+    source_link: null,
+    backStyles: null,
+    project_img: null
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setProject({
+      title: content.title,
+      details: content.details,
+      description: content.description,
+      source_link: content.source_link,
+      backStyles: content.backStyles,
+      project_img: content.project_img
+    });
+    // setLoading(false);
+  }, [content]);
+
   const {
     title,
     details,
@@ -8,30 +33,34 @@ const Summary = ({ content }) => {
     source_link,
     backStyles,
     project_img
-  } = content;
-
-  console.log(project_img);
+  } = project;
 
   return (
     <div className='summary'>
-      <div className='showcase'>
-        <a href='#!'>
-          <img src={project_img} alt={title} />
-        </a>
-      </div>
-      <div className='details'>
-        <h3 style={backStyles}>{title}</h3>
-        <h4>{details}</h4>
-        <p>{description}</p>
-        <a
-          href={source_link}
-          style={backStyles}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Source Code
-        </a>
-      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <div className='showcase'>
+            <a href='#!'>
+              <img src={project_img} alt={title} />
+            </a>
+          </div>
+          <div className='details'>
+            <h3 style={backStyles}>{title}</h3>
+            <h4>{details}</h4>
+            <p>{description}</p>
+            <a
+              href={source_link}
+              style={backStyles}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Source Code
+            </a>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 };
